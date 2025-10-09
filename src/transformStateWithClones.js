0b1744e54eb0ddc63364923ca62f7ea83f587701
Marcelo Kirino <mhkirino@hotmail.com>
2025-10-10 05:35:37 +0900

    First try to solve the exercise

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,22 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  switch (actions.type) {
+    case 'addPropreties':
+      for (const action of actions) {
+        Object.assign(state, action.extraDate);
+      }
+    case 'removeProperties':
+      for (const key of actions) {
+        delete state[key];
+      }
+
+    case 'clear':
+      for (const key in actions) {
+        delete state[key];
+      }
+  }
+
 }
 
 module.exports = transformStateWithClones;
